8af66b1c3bec157b184d3b26df35f0ec9c2ef388
benrhodes477 <72891041+benrhodes477@users.noreply.github.com>
2020-10-14 15:36:11 -0500

    updated

--- geojson.js
+++ geojson.js
@@ -12,168 +12,209 @@ var json = {
     {
       "type": "Feature",
       "properties": {
+        "marker-color": "#ff0000",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Stay Home! Stay Healthy! Sign",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50482988796/in/dateposted-public/\" title=\"Stay Home! Stay Healthy! Sign\"><img src=\"https://live.staticflickr.com/65535/50482988796_0e003ccfb0_m.jpg\" width=\"194\" height=\"240\" alt=\"Stay Home! Stay Healthy! Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.69995427131653,
+          40.81403658978752
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#0040ff",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Socially Distanced Scooters",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50483147342/in/dateposted-public/\" title=\"Socially Distanced Scooters\"><img src=\"https://live.staticflickr.com/65535/50483147342_82eef669a4_m.jpg\" width=\"180\" height=\"240\" alt=\"Socially Distanced Scooters\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70280814170836,
+          40.81484858063401
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#00ff11",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Thank You Sign",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50482304008/in/dateposted-public/\" title=\"Thank You Sign\"><img src=\"https://live.staticflickr.com/65535/50482304008_4481a89ac7_m.jpg\" width=\"180\" height=\"240\" alt=\"Thank You Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.69957876205444,
+          40.81371991066367
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ff7300",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Remote Employment Sign",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50482308453/in/dateposted-public/\" title=\"Remote Employment Sign\"><img src=\"https://live.staticflickr.com/65535/50482308453_dfaeea4aa7_m.jpg\" width=\"240\" height=\"126\" alt=\"Remote Employment Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70247554779053,
+          40.8140203498692
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#ee00ff",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Mask Art Poster",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50482313898/in/dateposted-public/\" title=\"Mask Art Poster\"><img src=\"https://live.staticflickr.com/65535/50482313898_ca54e78fbd_m.jpg\" width=\"180\" height=\"240\" alt=\"Mask Art Poster\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70232534408568,
+          40.813492550360536
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#00ffee",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Service Use Sign",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50483034126/in/dateposted-public/\" title=\"Service Use Sign\"><img src=\"https://live.staticflickr.com/65535/50483034126_e08382a025_m.jpg\" width=\"180\" height=\"240\" alt=\"Service Use Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.7010486125946,
+          40.81403658978752
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#eeff00",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Dine-In Sign",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50483039206/in/dateposted-public/\" title=\"Dine-In Sign\"><img src=\"https://live.staticflickr.com/65535/50483039206_09cb3fb191_m.jpg\" width=\"191\" height=\"240\" alt=\"Dine-In Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70131683349608,
+          40.81374427064994
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#6200a3",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Face Mask Required Sign",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50482331018/in/dateposted-public/\" title=\"Face Mask Required Sign\"><img src=\"https://live.staticflickr.com/65535/50482331018_959a56d561_m.jpg\" width=\"180\" height=\"240\" alt=\"Face Mask Required Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.7016816139221,
+          40.815749878835646
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#008026",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "Lonely Table",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50483047576/in/dateposted-public/\" title=\"Lonely Table\"><img src=\"https://live.staticflickr.com/65535/50483047576_3734ae82e8_m.jpg\" width=\"240\" height=\"180\" alt=\"Lonely Table\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70236825942993,
+          40.81515713454992
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#000000",
+        "marker-size": "medium",
+        "marker-symbol": "",
+        "title": "We're Open! Sign",
+        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/190032975@N06/50483050966/in/dateposted-public/\" title=\"We&#x27;re Open! Sign\"><img src=\"https://live.staticflickr.com/65535/50483050966_d41585d796_m.jpg\" width=\"240\" height=\"180\" alt=\"We&#x27;re Open! Sign\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -96.70377373695372,
+          40.81465370373719
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
         "stroke": "#555555",
         "stroke-width": 2,
         "stroke-opacity": 1,
         "fill": "#555555",
         "fill-opacity": 0.5,
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg",
-        "title": "Nebraska State Capitol",
-        "description": "Some description about something that happened at the capitol"
+        "title": "Area of Focus"
       },
       "geometry": {
         "type": "Polygon",
         "coordinates": [
           [
             [
-              -96.70105934143066,
-              40.806971850092815
+              -96.7040741443634,
+              40.81357375055825
             ],
             [
-              -96.69826984405518,
-              40.806971850092815
+              -96.69949293136597,
+              40.81357375055825
             ],
             [
-              -96.69826984405518,
-              40.809131953785965
+              -96.69949293136597,
+              40.81585543548167
             ],
             [
-              -96.70105934143066,
-              40.809131953785965
+              -96.7040741443634,
+              40.81585543548167
             ],
             [
-              -96.70105934143066,
-              40.806971850092815
-            ]
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "marker-color": "#ee05ff",
-        "marker-size": "medium",
-        "marker-symbol": "",
-        "ingredients": "rhubarb, strawberries, egg, milk, butter, so much sugar",
-        "thanks": "thanks to Linnea for giving me a pie",
-        "title": "Starting Point",
-        "image": "https://s3.amazonaws.com/omeka-net/53984/archive/square_thumbnails/e5847940c8baa5e9e50ad71ee468ba0e.jpg?AWSAccessKeyId=AKIAI3ATG3OSQLO5HGKA&Expires=1600905600&Signature=PapYafLVt%2BGfMiI%2F06Ibpay%2FXPk%3D"
-      },
-      "geometry": {
-        "type": "Point",
-        "coordinates": [
-          -96.70299053192137,
-          40.81015513625622
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "stroke": "#ff5100",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "title": "Path to the library",
-        "image": "https://1.bp.blogspot.com/-WbftUEch0t0/X2DQmPdyksI/AAAAAAAAAIs/JHH1hwcqN0IVWsR8KCYabom1fqoweHBFACLcBGAsYHQ/w1684-h1069-p-k-no-nu/hearts.jpg"
-      },
-      "geometry": {
-        "type": "LineString",
-        "coordinates": [
-          [
-            -96.69979333877563,
-            40.80922127986828
-          ],
-          [
-            -96.69986844062804,
-            40.811389430620544
-          ],
-          [
-            -96.6997504234314,
-            40.811405671182825
-          ],
-          [
-            -96.6996967792511,
-            40.81468618325975
-          ],
-          [
-            -96.69984698295592,
-            40.8146943031379
-          ],
-          [
-            -96.69984698295592,
-            40.816837916207085
-          ],
-          [
-            -96.70262575149535,
-            40.81687851427114
-          ]
-        ]
-      }
-    },
-    {
-      "type": "Feature",
-      "properties": {
-        "stroke": "#d4ff00",
-        "stroke-width": 2,
-        "stroke-opacity": 1,
-        "fill": "#0060ff",
-        "fill-opacity": 0.2,
-        "title": "Love Library",
-        "image": "<a data-flickr-embed=\"true\" href=\"https://www.flickr.com/photos/189753833@N07/50237618773/in/album-72157715528007683/\" title=\"yawn_connie\"><img src=\"https://live.staticflickr.com/65535/50237618773_c9052299b5_w.jpg\" width=\"300\" height=\"400\" alt=\"yawn_connie\"></a><script async src=\"//embedr.flickr.com/assets/client-code.js\" charset=\"utf-8\"></script>",
-        "random": "Some other random field"
-      },
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -96.70274376869202,
-              40.817040906278805
-            ],
-            [
-              -96.70216441154479,
-              40.81700030831414
-            ],
-            [
-              -96.70187473297119,
-              40.817235776163265
-            ],
-            [
-              -96.70193910598755,
-              40.81753619944697
-            ],
-            [
-              -96.70217514038086,
-              40.81769858984467
-            ],
-            [
-              -96.70218586921692,
-              40.818104564099706
-            ],
-            [
-              -96.70297980308533,
-              40.81808832517719
-            ],
-            [
-              -96.70300126075745,
-              40.817609275175144
-            ],
-            [
-              -96.70323729515076,
-              40.81742252593206
-            ],
-            [
-              -96.70299053192137,
-              40.81704902586876
-            ],
-            [
-              -96.70274376869202,
-              40.817040906278805
+              -96.7040741443634,
+              40.81357375055825
             ]
           ]
         ]
       }
     }
   ]
-}
